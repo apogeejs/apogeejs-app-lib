51f2e0d154d852778a51896c207c7dfacb716c99
sutter-dave <sutter@intransix.com>
2021-04-28 18:04:30 -0700

    update web request cell

--- src/components/WebRequestComponent.js
+++ src/components/WebRequestComponent.js
@@ -9,6 +9,11 @@ export default class WebRequestComponent extends FormInputBaseComponent {
 }
 
 const DATA_MEMBER_FUNCTION_BODY = `
+return WebRequestCell.formResultToRequest(formResult);
+`
+
+__globals__.WebRequestCell = {};
+WebRequestCell.formResultToRequest = function(formResult) {
     let url = formResult.url;
     if(url) {
         //construct options
@@ -33,68 +38,19 @@ const DATA_MEMBER_FUNCTION_BODY = `
             options.headers = headers;
         }
 
-        return fetch(url,options).then(response => {
-            //store meta data for response
-            let meta = {};
-            meta.status = response.status;
-            meta.headers = {};
-            for (let pair of response.headers.entries()) {
-            meta.headers[pair[0]] = pair[1];
-            }
-            
-            //save the body
-            let responseBodyPromise;
-            let bodyFormat;
-            if(formResult.outputFormat == "mime") {
-                let contentType = meta.headers["content-type"];
-                if((conttentType)&&(contentType.startsWith("application/json"))) bodyFormat = "json";
-                else if(contentType !== undefined) bodyFormat = "text";
-                else bodyFormat = "none";
-            }
-            else if(formResult.outputFormat == "json") bodyFormat = "json";
-            else if(formResult.outputFormat == "none") bodyFormat = "none";
-            else bodyFormat = "text";
-            
-            switch(bodyFormat) {
-                case "text":
-                    responseBodyPromise = response.text();
-                    break;
-                    
-                case "json": 
-                    responseBodyPromise = response.json();
-                    break;
-                    
-                case "none":
-                    responseBodyPromise = Promise.resolve("");
-                    break;
-            }
-            
-            return responseBodyPromise.then(body => {
-                let value = {
-                    meta: meta,
-                    body: body
-                }
-                if((response.ok)||(formResult.onError == "noError")) {
-                    return value;
-                }
-                else {
-                    let msg = "Error in request. Status code: " + response.status;
-                    if(response.statusText) msg += "; " + response.statusText
-                    let error = new Error(msg);
-                    error.valueData = {
-                        value: value,
-                        nominalType: "application/json",
-                        stringified: false
-                    }
-                    return Promise.reject(error)
-                }
-            })
-        })
+        //make the request, with apogee specific error handling
+        let bodyFormat = formResult.outputFormat;
+        let saveMetadata = true;
+        let noFailedRequestError = (formResult.onError == "noError");
+        return apogeeutil.httpRequest(url,options,bodyFormat,saveMetadata,noFailedRequestError);
     }
     else {
-        return apogeeutil.INVALID_VALUE;
+        return {
+            meta: "",
+            body: ""
+        };
     }
-`
+}
 
 //this defines the hardcoded type we will use
 let dataMemberTypeName = "apogee.WebRequestMember";
