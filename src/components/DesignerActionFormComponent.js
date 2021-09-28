7ab569af3543e2c5768a37b99186b1eb292ebbf0
sutter-dave <sutter@intransix.com>
2021-09-28 12:53:17 -0700

    more cleanup for new messenger

--- src/components/DesignerActionFormComponent.js
+++ src/components/DesignerActionFormComponent.js
@@ -2,39 +2,6 @@ import Component from "/apogeejs-app-lib/src/component/Component.js";
 import {getFormComponentDefaultMemberJson} from "/apogeejs-app-lib/src/components/formInputComponentUtils.js";
 import {defineHardcodedDataMember} from "/apogeejs-model-lib/src/apogeeModelLib.js";
 
-function onSubmitCodeUpdate(component,submitCode) {
-    let onSubmitFunction;
-    if((submitCode === undefined)&&(submitCode === null)) submitCode = "";
-    
-    try {
-        //create the validator function
-        onSubmitFunction = new Function("cmdMsngr","formValue","formObject",submitCode);
-    }
-    catch(error) {
-        if(error.stack) console.error(error.stack);
-        onSubmitFunction = error;
-    }
-
-    component.setField("onSubmitFunction",onSubmitFunction);
-}
-
-function onCancelCodeUpdate(component,cancelCode) {
-    let onCancelFunction;
-    if((cancelCode === undefined)&&(cancelCode === null)) cancelCode = "";
-    
-    try {
-        //create the validator function
-        onCancelFunction = new Function("cmdMsngr","formObject",cancelCode);
-    }
-    catch(error) {
-        if(error.stack) console.error(error.stack);
-        onCancelFunction = error;
-    }
-
-    component.setField("onCancelFunction",onCancelFunction);
-}
-
-
 const DATA_MEMBER_FUNCTION_BODY = `
 if(formResult) return apogeeui.ConfigurablePanel.getGeneratedFormLayout(formResult);
 else return [];
@@ -50,19 +17,9 @@ const DesignerActionFormComponentConfig = {
     defaultComponentJson: {
         type: "apogeeapp.DesignerActionFormCell",
         fields: {
-            allowInputExpressions: true,
-            onSubmitCode: "",
-            onCancelCode: ""
+            allowInputExpressions: true
         }
-    },
-    fieldFunctions: {
-		onSubmitCode: {
-			fieldChangeHandler: onSubmitCodeUpdate 
-		},
-        onCancelCode: {
-			fieldChangeHandler: onCancelCodeUpdate 
-		}
-	}
+    }
 }
 export default DesignerActionFormComponentConfig;
 
