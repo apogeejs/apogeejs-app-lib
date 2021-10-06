064bf259731172285d77c560a84a5f31bf243254
sutter-dave <sutter@intransix.com>
2021-10-05 17:03:20 -0700

    adding types for apogee module - not programmed yet

---
+++ src/references/NodeApogeeModuleEntry.js
@@ -0,0 +1,54 @@
+import ReferenceEntry from "/apogeejs-app-lib/src/references/ReferenceEntry.js";
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+export default class NpmModuleEntry extends ReferenceEntry {
+    
+    constructor(referenceList,referenceData) {
+        super(referenceList,referenceData,NpmModuleEntry.REFERENCE_TYPE_INFO);
+
+    }
+
+    /** This method loads the actual link. */
+    implementationLoadEntry(onLoad,onError) {
+
+        try {
+            let module = require(this.getUrl());
+            if((module)&&(module.initApogeeModule)) module.initApogeeModule();
+            this.setField("module",module);
+            
+            //we need to call this asynchronously
+            setTimeout(onLoad,0);
+        }
+        catch(error) {
+            if(error.stack) console.error(error.stack);
+            
+            //we need to call this asynchronously
+            setTimeout(onError(error),0);
+        }
+
+    }
+    
+    /** This method removes the link. */
+    removeEntry() {
+        //allow for an optional module remove step
+        let module = this.getField("module");
+        if(module) {
+            if(module.removeApogeeModule) module.removeApogeeModule();
+
+            this.clearField("module");
+        }
+        
+        //we aren't really removing it...
+        //require.undef(this.url);
+
+        return true;
+    }
+    
+}
+
+NpmModuleEntry.REFERENCE_TYPE = "npm module";
+
---
+++ src/references/WebApogeeModuleEntry.js
@@ -0,0 +1,111 @@
+import ReferenceEntry from "/apogeejs-app-lib/src/references/ReferenceEntry.js";
+import CommandManager from "/apogeejs-app-lib/src/commands/CommandManager.js";
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+export default class EsModuleEntry extends ReferenceEntry {
+    
+    constructor(referenceList,referenceData) {
+        super(referenceList,referenceData,EsModuleEntry.REFERENCE_TYPE_INFO);
+    }
+            
+    /** This method loads the actual link. */
+    implementationLoadEntry(onLoad,onError,workspaceManager) {
+        let localOnLoad = (module) => {
+            if(module) {
+                if((module.default)&&(module.default.initApogeeModule)) module.default.initApogeeModule();
+            
+                let commandData = {
+                    type: "setEsModule",
+                    entryType: this.referenceType,
+                    url: this.getUrl(),
+                    module: module
+                };
+                workspaceManager.getApp().executeCommand(commandData);
+                onLoad();
+            }
+            else {
+                onError("Unknown error: Module not properly loaded. " + this.getUrl());
+            }
+
+        }
+
+        //load the module
+        var moduleLoadPromise = import(this.getUrl()).then(localOnLoad).catch(onError);
+    }
+    
+    /** This method removes the link. This returns a command result for the removed link. */
+    removeEntry() {
+        //allow for an optional module remove step
+        let module = this.getField("module");
+        if(module) {
+            if((module.default)&&(module.default.removeApogeeModule)) module.default.removeApogeeModule();
+            this.clearField("module");
+
+            ///////////////////////////////////////////////////
+            //temp import logic
+            apogeeplatform.removeImport(this.getNickname());
+            //end temp import logic
+            /////////////////////////////////////////////////////
+        }
+
+        return true;
+    }
+    
+}
+
+EsModuleEntry.REFERENCE_TYPE = "es module";
+
+//=====================================
+//Load Module Command
+// These are commands run to update the status of the link after loading completes
+//=====================================
+
+/*
+ *
+ * Command JSON format:
+ * {
+ *   "type":"setEsModule",
+ *   "entryType":(entry type),
+ *   "url":(url),
+ *   "module":(the module),
+ * }
+ * 
+ */ 
+
+let setesmodule = {};
+
+//No undo command. Only the original call needs to be undone.
+//setesmodule.createUndoCommand = function(workspaceManager,commandData) {
+
+setesmodule.executeCommand = function(workspaceManager,commandData) {
+    
+    let referenceManager = workspaceManager.getMutableReferenceManager();
+
+    let refEntryId = referenceManager.lookupRefEntryId(commandData.entryType,commandData.url);
+    if(!refEntryId) throw new Error("Reference entry not found. " + commandData.entryType + ":" + commandData.url);
+
+    let referenceEntry = referenceManager.getMutableRefEntryById(refEntryId);
+    if(!referenceEntry) throw new Error("Reference entry not found. refEntryId: " + refEntryId);
+    
+    referenceEntry.setField("module",commandData.module);
+
+    ///////////////////////////////////////////
+    // temp import logic
+    let importName = referenceEntry.getNickname();
+    apogeeplatform.addImport(importName,commandData.module);
+    //end temp import logic
+    /////////////////////////////////////////////
+}
+
+setesmodule.commandInfo = {
+    "type": "setEsModule",
+    "targetType": "referenceEntry",
+    "event": "updated"
+}
+
+CommandManager.registerCommand(setesmodule);
+
